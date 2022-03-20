04e58f821873c33dc7c3a827c2c5b9940e303b86
SIHAM TAHI <gs_tahi@esi.dz>
2022-03-20 14:42:45 +0100

    adding Entity frame db

---
+++ superHeroAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace superHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+         public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+
+        // this is our datacontext 
+        public DbSet<SuperHero> SuperHeroes{ get; set; }
+    }
+}
